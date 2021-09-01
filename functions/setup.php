bfec46c2553c60510e8982b2224dc57fd0f327dd
Santiago <santiagomartinezlt@gmail.com>
2021-09-01 17:00:29 +0200

    Maps Terminado

--- functions/setup.php
+++ functions/setup.php
@@ -88,3 +88,11 @@ register_nav_menus( array(
     'third' => __( 'Footer Menu 02', 'demo' ),
     'fourth' => __( 'Footer Menu 03', 'demo' ),
 ));
+
+/*----------------------------------*/
+/*---------- SLUG STRING -----------*/
+/*----------------------------------*/
+function slugify($string) {
+  $slug = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $string)));
+  return $slug;
+}
