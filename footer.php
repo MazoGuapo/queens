03e83a5e2398a8f25a8a5e0589ef84b50c5a20ff
Santiago <santiagomartinezlt@gmail.com>
2021-09-01 16:10:32 +0200

    Auto stash before merge of "main" and "origin/main"

--- footer.php
+++ footer.php
@@ -15,23 +15,29 @@
         <img src="<?php bloginfo('template_url')?>/src/assets/images/footer_bg.jpg">
     </div>
 
-    <div class="grid ipad:grid-cols-4 grid-cols-1">
-        <div class="grid_logo">
-            <img src="<?php bloginfo('template_url')?>/src/assets/images/footer_logo_white.jpg">
-            <p></p>
+    <div class="grid_footer gap-y-24">
+
+        <div class="grid_logo flex justify-start items-start">
+            <img class="mr-10" src="<?php bloginfo('template_url')?>/src/assets/images/footer_logo_white.png">
+            <p class="font-primary font-thin text-primary text-h1 max-w-40"><?php echo $footer['title_footer']?></p>
         </div>
-        <div class="grid_contact">
-            <p class="title"></p>
-            <p class=""></p>
+
+        <div class="grid_contact flex flex-col justify-between">
+            <p class="font-secondary font-light text-primary">Oficina de Ventas</p>
+            <div class="address">
+                <?php echo $footer['contact_footer']; ?>
+            </div>
         </div>
-        <div class="grid_legal">
+
+        <div class="grid_legal flex flex-col justify-between">
+            <p class="font-secondary font-light text-primary">Legal</p>
             <ul class="menu">
-                <li class="title">Legal</li>
+
                 <?php foreach( $menu_items as $menu_item ) {
 				$link = $menu_item->url;
 				$title = $menu_item->title;
 				?>
-                <li>
+                <li class="font-primary font-light text-primary text-h4 pb-1">
                     <a href="<?php echo $link; ?>">
                         <?php echo $title?>
                     </a>
@@ -39,12 +45,23 @@
                 <?php } ?>
             </ul>
         </div>
-        <div class="grid_copyright"></div>
-        <div class="grid_social">
+
+        <div class="grid_copyright">
+            <p class="flex font-thin text-primary">
+                <span class="pr-2">Managed by</span>
+                <img class="object-contain pr-2" src="<?php bloginfo('template_url')?>/src/assets/images/logo_asg.png">
+                <span class="pr-2">|</span>
+                <span class="pr-2">Design & Marketing by</span>
+                <img class="object-contain pr-2" src="<?php bloginfo('template_url')?>/src/assets/images/logo_weplan.png">
+            </p>
+
+        </div>
+
+        <div class="grid_social flex">
             <?php foreach($redes as $red) { ?>
-                <a href="<?php echo $red['link']?>">
-                    <img src="<?php echo $red['icono']?>">
-                </a>
+            <a class="pr-4" href="<?php echo $red['link']['sizes']['theme_full']?>">
+                <img src="<?php echo $red['icono']['sizes']['theme_full']?>">
+            </a>
             <?php } ?>
         </div>
     </div>
@@ -53,4 +70,5 @@
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
